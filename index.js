c17590aa88c0d8ed19040584693222fb1a317d74
cravsky <cravsky@gmail.com>
2024-09-12 12:24:40 +0200

    Created prompt

--- index.js
+++ index.js
@@ -17,16 +17,18 @@ app.post('/interpret', async (req, res) => {
     const completion = await openai.chat.completions.create({
         messages: [{
             "role": "system",
-            "content": "You are a helpful assistant."
+            "content": "You are a fortune teller. Provide a dream interpretation. No bullet points. No ambiguity. Let your answer be a narrative. Finish with a quote. Predict what the future holds."
         },
         {
             "role": "user",
             "content": text
         }],
-        model: "gpt-4o-mini"
+        model: "gpt-4o-mini",
+        max_tokens: 2048,
     })
+    console.log(completion)
     console.log(completion.choices[0].message.content)
-    
+
     res.send({ response: completion.choices[0].message.content })
 })
 
